cfeae8da051c6bafde936cd812d575d107d0fb90
Javoopls <ja.vivancoa@duocuc.cl>
2021-09-16 19:54:50 -0300

    loading

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,7 @@ export class LoginPage implements OnInit {
 
   user:string
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, public loadingController: LoadingController) {}
 
   resetPass(){
     this.router.navigate(['/password']);
@@ -18,16 +19,25 @@ export class LoginPage implements OnInit {
 
   home(){
 
+    this.presentLoading();
     let navigationExtra: NavigationExtras={
       state:{user: this.user}
     }
-
     this.router.navigate(['/home'],navigationExtra);
   }
   
   ngOnInit() {
   }
 
-  
+  async presentLoading() {
+    const loading = await this.loadingController.create({
+      cssClass: 'my-custom-class',
+      message: 'Iniciando Sesión...',
+      duration: 1000
+    });
+    await loading.present();
+
+    const { role, data } = await loading.onDidDismiss();
+  } 
 
 }
